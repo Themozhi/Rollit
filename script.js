0def5a09855515dc0cf629eec0d4972556a457e1
Themozhi <beyondinfinity121@gmail.com>
2022-01-31 00:53:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+const player0e=document.querySelector('.player--0');
+const player1e=document.querySelector('.player--1');
+const score0e=document.querySelector('#score--0');
+const score1e=document.getElementById('score--1'); 
+const current0e=document.getElementById('current--0');
+const current1e=document.getElementById('current--1');
+const dicee=document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll=document.querySelector('.btn--roll');
+const btnHold=document.querySelector('.btn--hold');
+
+let scores,currentScore,activePlayer,playing;
+
+const init=function(){
+    scores=[0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0e.textContent=0;
+    score1e.textContent=0;
+    current0e.textContent=0;
+    current1e.textContent=0;
+    dicee.classList.add('hidden');
+    player0e.classList.remove('player--winner');
+    player1e.classList.remove('player-winner');
+    player0e.classList.add('player--active');
+    player1e.classList.remove('player--active');
+
+};
+init();
+
+const switchPlayer=function(){
+    document.getElementById(`current--${activePlayer}`).textContent=0;
+    currentScore=0;
+    activePlayer=activePlayer===0 ? 1 : 0;
+    player0e.classList.toggle('player--active');
+    player1e.classList.toggle('player--active');
+};
+btnRoll.addEventListener('click',function(){ 
+    if (playing){
+    const dice=Math.trunc(Math.random()*6)+1;
+    dicee.classList.remove('hidden');
+    dicee.src=`dice-${dice}.png`;
+    if (dice!==1){
+        currentScore+=dice;
+        document.getElementById(`current--${activePlayer}`).textContent=currentScore;
+
+    }else{
+        switchPlayer()
+    }
+}});
+btnHold.addEventListener('click',function(){
+    if (playing){
+    scores[activePlayer]+=currentScore;
+    console.log(scores[activePlayer]);
+    document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer];
+    if (scores[activePlayer]>=50){
+        playing=false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        dicee.classList.add('hidden');
+    }else{
+        switchPlayer()
+    }
+}});
+btnNew.addEventListener('click',function(){
+    init();
+
+});
